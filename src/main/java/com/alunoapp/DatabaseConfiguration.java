d84bb8793d445b85ecf88cc971f01ac6eb4bfba1
Alfredo Torres <amtrosul@hotmail.com>
2022-02-10 11:13:46 -0300

    db config

--- src/main/java/com/alunoapp/DatabaseConfiguration.java
+++ src/main/java/com/alunoapp/DatabaseConfiguration.java
@@ -1,48 +1,21 @@
 package com.alunoapp;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@Profile("dev")
 public class DatabaseConfiguration {
 
 	@Bean
-	public BasicDataSource dataSource() throws URISyntaxException {
-		String dbUrl = System.getenv("JDBC_DATABASE_URL");
-		String username = System.getenv("JDBC_DATABASE_USERNAME");
-		String password = System.getenv("JDBC_DATABASE_PASSWORD");
-
-		BasicDataSource basicDataSource = new BasicDataSource();
-		basicDataSource.setUrl(dbUrl);
-		basicDataSource.setUsername(username);
-		basicDataSource.setPassword(password);
-
-		return basicDataSource;
-	}
-
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.POSTGRESQL);
-		adapter.setShowSql(false);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
-		adapter.setPrepareConnection(true);
-		return adapter;
-	}
-
-	@Bean
-	public DataSource dataSourceMySql() {
+	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		// dataSource.setDriverClassName("org.postgresql.Driver");
@@ -53,7 +26,7 @@ public class DatabaseConfiguration {
 	}
 
 	@Bean
-	public JpaVendorAdapter jpaVendorAdapterMySql() {
+	public JpaVendorAdapter jpaVendorAdapter() {
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(false);
